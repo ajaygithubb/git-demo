f5ed1bc92abf3c484f7052dc58b735f4198586d5
Ajay <akajykumar0993@gmail.com>
2023-05-23 15:02:55 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechie/app/gitdemo/GitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.app.gitdemo;
+
+public class GitDemoApplication {
+	public void addData(String data) {
+		System.out.println("data =" + data);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Hellow World");
+	}
+
+}
